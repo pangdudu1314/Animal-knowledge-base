bcfe0fc47c13f3fe53a4d47bd4d7cd2936db054d
liu26437775 <zxliuzx@163.com>
2019-05-12 23:04:59 +0800

    修改图片上传位置

--- src/main/java/com/li/utils/Config.java
+++ src/main/java/com/li/utils/Config.java
@@ -1,7 +1,9 @@
 package com.li.utils;
 
 public class Config {
+  //项目中的文件路径
   public static String ABSOLUTE_PATH = "E:\\git\\Animal-knowledge-base\\src\\main\\webapp\\images";
-  public static String IMP_PATH = "E:\\git\\Animal-knowledge-base\\src\\main\\webapp\\images";
+  //图片识图导入的路径
+  public static String IMP_PATH = "D:\\images";
 
 }
