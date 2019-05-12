ec6839388626f90e3e5dbe86cff633d39da45a41
liu26437775 <zxliuzx@163.com>
2019-05-12 22:47:28 +0800

    sql脚本

---
+++ db.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `animal_visits` (
+  `animal_name` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '动物名称',
+  `animal_visits` decimal(10,0) DEFAULT NULL COMMENT '动物访问量',
+  `week_day` varchar(100) COLLATE utf8_bin DEFAULT '' COMMENT '周几'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='动物访问量';
+
+
+
+
+CREATE TABLE `animal_check` (
+  `id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `animal_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `data_from` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `status` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `animal_image` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `animal_intro` varchar(4000) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+CREATE TABLE `admin_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
